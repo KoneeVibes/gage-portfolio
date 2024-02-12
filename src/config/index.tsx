8d93b42b569df5ec8aff02aa4a6fcfc228858383
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-02-12 16:25:49 +0100

    fixing download problems

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -85,7 +85,7 @@ export const projectdetails = [
             },
             {
                 text: "12-13-2021 - Roblox Award Article by Roblox (English)",
-                href: "/../files/Roblox_article_by_Roblox_English.pdf",
+                href: "/../files/Roblox_article_by_Roblox_English.docx",
                 download: "Roblox_article_by_Roblox_English",
             },
             {
